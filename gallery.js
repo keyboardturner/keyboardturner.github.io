9a268df3c098ba113933e6d4b6e0fde2c59dc33e
keyboardturner <20036792+keyboardturner@users.noreply.github.com>
2023-12-13 21:44:14 -0800

    test

---
+++ gallery.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const galleryContainer = document.querySelector(".gallery");
+
+    // Replace 'images/' with the path to your images folder
+    const imagePath = "assets/images/";
+
+    // List your image filenames here
+    const imageList = [
+        "berlin-1.jpg",
+        "berlin-2.jpg",
+        "berlin-3.jpg",
+        "charlestonchews.jfif",
+        // Add more filenames as needed
+    ];
+
+    imageList.forEach((imageName) => {
+        const imgElement = document.createElement("img");
+        imgElement.src = imagePath + imageName;
+        imgElement.alt = "Artwork";
+        galleryContainer.appendChild(imgElement);
+    });
+});
